2ee9389dee38d3e43946ec49ffa240bbdffa058b
Stan Silvert <ssilvert@redhat.com>
2020-03-12 09:31:40 -0400

    snowpack + React

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from '/web_modules/react.js';
+import ReactDOM from '/web_modules/react-dom.js';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
